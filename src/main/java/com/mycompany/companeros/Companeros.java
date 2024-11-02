fa1892df86b3cc098815c8027959a62f509a71f3
User <User@192.168.100.203>
2024-11-01 18:03:45 -0600

    Primer Tarea Módulo 1-M

---
+++ src/main/java/com/mycompany/companeros/Companeros.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.companeros;
+
+/**
+ *
+ * @author User
+ */
+public class Companeros {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Eduardo Guillén", "David Inestroza", "Diana Martínez", "Ramón Cueva", "Irene Sánchez", "Keyla Hérnandez", "Isaac López", "Kevin Pineda", "Luis Alemán", "Nélson Fernández"};
+
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
